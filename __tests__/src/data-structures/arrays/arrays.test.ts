6ef2beffc1ea2f06694b303c27bb42d01011949a
dyhalmeida <dyhalmeida@gmail.com>
2024-01-25 18:18:43 -0300

    test(array): should access the values of an array through @@iterator values and return value

--- __tests__/src/data-structures/arrays/arrays.test.ts
+++ __tests__/src/data-structures/arrays/arrays.test.ts
@@ -250,4 +250,15 @@ describe('Arrays', () => {
         expect(entries.next().done).toBeTruthy()
     })
 
+    it('should access the values of an array through @@iterator values and return value', () => {
+        const numbers = [1,2,3,4,5]
+        const entries = numbers.values()
+        expect(entries.next().value).toBe(1)
+        expect(entries.next().value).toBe(2)
+        expect(entries.next().value).toBe(3)
+        expect(entries.next().value).toBe(4)
+        expect(entries.next().value).toBe(5)
+        expect(entries.next().done).toBeTruthy()
+    })
+
 })
