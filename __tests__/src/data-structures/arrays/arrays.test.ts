21ff85becb1d9f3107b126d074689e9590e028ac
dyhalmeida <dyhalmeida@gmail.com>
2024-01-26 14:59:15 -0300

    test(array): should sort the array using Array.sort

--- __tests__/src/data-structures/arrays/arrays.test.ts
+++ __tests__/src/data-structures/arrays/arrays.test.ts
@@ -295,4 +295,14 @@ describe('Arrays', () => {
         expect(numbers).toStrictEqual([1,4,5,4,5,6,7,8])
     })
 
+    it('should sort the array using Array.sort', () => {
+        const compare = (a: number, b: number) => {
+            if (a === b) return 0
+            return a > b ? 1 : -1
+        }
+        const numbers = [15,14,13,12,11,10,9,8,7,6,5,4,3,2,1]
+        numbers.sort(compare)
+        expect(numbers).toStrictEqual([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15])
+    })
+
 })
