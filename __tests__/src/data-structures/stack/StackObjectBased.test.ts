7327e8b76824a4f28905f6562cbb51c84856dca1
dyhalmeida <dyhalmeida@gmail.com>
2024-02-09 13:20:28 -0300

    test(stack): should remove an element from the top of the stack

--- __tests__/src/data-structures/stack/StackObjectBased.test.ts
+++ __tests__/src/data-structures/stack/StackObjectBased.test.ts
@@ -10,3 +10,14 @@ describe('StackObejctBased', () => {
         expect(stack.peek()).toBe(3)
     })
 
+    it('should remove an element from the top of the stack', () => {
+        const stack = new StackObjectBased<number>()
+        stack.push(1)
+        stack.push(2)
+        stack.push(3)
+        expect(stack.peek()).toBe(3)
+        const removed = stack.pop()
+        expect(removed).toBe(3)
+        expect(stack.peek()).toBe(2)
+    })
+
